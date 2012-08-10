53657171155a465d27ebd00732f66eaaa78ea787
anara <anara@ANARA-LAP-WIN7.nice.com>
2012-08-10 19:52:34 +0300

    dvirgu

---
+++ TaskManagmentSystemBackEndModule/src/main/java/com/anardvir/backend/impl/TaskManagementBEServiceImpl.java
@@ -0,0 +1,31 @@
+package com.anardvir.backend.impl;
+
+import java.util.List;
+
+import com.anardvir.backend.objects.TaskBE;
+import com.anardvir.backend.objects.UserBE;
+import com.anardvir.backend.services.TaskManagerBEService;
+
+public class TaskManagementBEServiceImpl implements TaskManagerBEService {
+
+	public void assignUserToTask(UserBE user, TaskBE task) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public List<TaskBE> getUserTasks(UserBE user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void removeTask(int taskId) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void updateTask(TaskBE task) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
